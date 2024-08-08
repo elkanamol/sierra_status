259e4af1c58de4bc5efc72d4ddf32f25b937f5ed
Elkana Molson <elkanamol@gmail.com>
2024-08-08 18:25:29 +0300

    upate the files
    Creating a new project to support customers need to get quickly status about Sierra M.2 modules.
    this is OS agnostic CLI package that can install on the system and run independently.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # em-mc_status_script
+
  Script to get the SIerra EM/MC modules to get first analysis
---
+++ setup.py
@@ -0,0 +1,62 @@
+"""
+Defines the setup configuration for the sierra_flasher package.
+
+This setup configuration includes the package metadata, such as the name, version, description, author, license, and URLs for the project. It also specifies the required Python version, the packages to be included, and the console script entry point.
+"""
+
+from setuptools import setup, find_packages
+import os
+
+version = "0.0.2"
+# User-friendly description from README.md
+current_directory = os.path.dirname(os.path.abspath(__file__))
+try:
+    with open(os.path.join(current_directory, "README.md"), encoding="utf-8") as f:
+        long_description = f.read()
+except Exception:
+    long_description = ""
+requirement_path = f"{current_directory}/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+
+"""
+Defines the setup configuration for the sierra-status package.
+
+This setup configuration includes the package metadata, such as the name, version, description, author, license, and URLs for the project. It also specifies the required Python version, the packages to be included, and the console script entry point.
+"""
+setup(
+    name="sierra_status",
+    version=version,
+    packages=find_packages(),
+    description="CLI tool for Sierra Wireless EM9xxx/EM7xxx modules to query status",
+    author="Elkana Molson",
+    author_email="elkanamol@gmail.com",
+    long_description=long_description,
+    license="MIT",
+    url="https://github.com/elkanamol/sierra_lte_5g_flasher",
+    project_urls={
+        "Documentation": "https://github.com/elkanamol/sierra_lte_5g_flasher/blob/main/README.md",
+        "Source": "https://github.com/elkanamol/sierra_lte_5g_flasher",
+        "Tracker": "https://github.com/elkanamol/sierra_lte_5g_flasher/issues",
+    },
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "sierra-status = sierra_status.src.cli:main",
+        ],
+    },
+    python_requires=">=3.8",  # Requires Python 3.8 and above
+)
+

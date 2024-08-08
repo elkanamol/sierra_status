3b2d9bfbd7b9367deda059c38e87bde8daa7e031
Elkana Molson <elkanamol@gmail.com>
2024-08-08 18:49:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ This setup configuration includes the package metadata, such as the name, versio
 from setuptools import setup, find_packages
 import os
 
-version = "0.0.2"
+version = "0.0.3"
 # User-friendly description from README.md
 current_directory = os.path.dirname(os.path.abspath(__file__))
 try:
